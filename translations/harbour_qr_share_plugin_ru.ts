f82188dd6fb4028f75f431b54074131407596571
Petr Tsymbarovich <petr@tsymbarovich.ru>
2015-03-08 14:26:09 +0300

    Russian translation

---
+++ translations/harbour_qr_share_plugin_ru.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name></name>
+    <message id="harbour-qr-share-plugin-id">
+        <location filename="../src/qrplugininfo.cpp" line="35"/>
+        <location filename="../src/qruploader.cpp" line="16"/>
+        <source></source>
+        <oldsource>QR-Code</oldsource>
+        <extracomment>Display name for QR share plugin</extracomment>
+        <translation>QR код</translation>
+    </message>
+    <message id="harbour-qr-share-plugin-regenerate-id">
+        <location filename="../qml/QRCodeShow.qml" line="104"/>
+        <source>Regenerate with edited data</source>
+        <extracomment>Generate with edited data</extracomment>
+        <translation>Сгенерировать заново</translation>
+    </message>
+    <message id="harbour-qr-share-plugin-save-pic-id">
+        <location filename="../qml/QRCodeShow.qml" line="83"/>
+        <source>Save in pictures</source>
+        <extracomment>Save function text</extracomment>
+        <translation>Сохранить в изображениях</translation>
+    </message>
+    <message id="harbour-qr-share-plugin-save-open-gallery-id">
+        <location filename="../qml/QRCodeShow.qml" line="93"/>
+        <source>Save and open in gallery</source>
+        <extracomment>Save and open function text</extracomment>
+        <translation>Сохранить и открыть в галерее</translation>
+    </message>
+    <message id="harbour-qr-share-plugin-header-id">
+        <location filename="../qml/QRCodeShow.qml" line="133"/>
+        <source>QR-code share</source>
+        <oldsource>Example Share</oldsource>
+        <extracomment>Header for QR-code plugin</extracomment>
+        <translation>Поделиться QR кодом</translation>
+    </message>
+</context>
+</TS>
