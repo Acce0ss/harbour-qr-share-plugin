93f694c26161c3dda36476a08a45b137ae98644e
Asser Lähdemäki <asser@lahdemaki.fi>
2015-09-05 11:56:27 +0300

    Updated swedish translation.

--- translations/harbour_qr_share_plugin_sv.ts
+++ translations/harbour_qr_share_plugin_sv.ts
@@ -1,39 +1,41 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="sv_SE">
+<TS version="2.0" language="sv_SE">
 <context>
     <name></name>
     <message id="harbour-qr-share-plugin-id">
-        <location filename="../src/qrplugininfo.cpp" line="35"/>
-        <location filename="../src/qruploader.cpp" line="16"/>
-        <source></source>
-        <oldsource>QR-Code</oldsource>
+        <location filename="src/qrplugininfo.cpp" line="62"/>
+        <source>Share as QR-code</source>
+        <oldsource>QR plugin</oldsource>
         <extracomment>Display name for QR share plugin</extracomment>
         <translation>Dela som QR-kod</translation>
     </message>
     <message id="harbour-qr-share-plugin-regenerate-id">
-        <location filename="../qml/QRCodeShow.qml" line="98"/>
+        <location filename="qml/QRCodeShow.qml" line="63"/>
         <source>Regenerate with edited data</source>
         <extracomment>Generate with edited data</extracomment>
         <translation>Koda om, med redigerad data</translation>
     </message>
     <message id="harbour-qr-share-plugin-save-pic-id">
-        <location filename="../qml/QRCodeShow.qml" line="77"/>
+        <location filename="qml/QRCodeShow.qml" line="73"/>
         <source>Save in pictures</source>
         <extracomment>Save function text</extracomment>
-        <translation>Spara i bilder</translation>
-    </message>
-    <message id="harbour-qr-share-plugin-save-open-gallery-id">
-        <location filename="../qml/QRCodeShow.qml" line="87"/>
-        <source>Save and open in gallery</source>
-        <extracomment>Save and open function text</extracomment>
-        <translation>Spara och öppna i galleri</translation>
+        <translation>Spara i bildmappen</translation>
     </message>
     <message id="harbour-qr-share-plugin-header-id">
-        <location filename="../qml/QRCodeShow.qml" line="127"/>
+        <location filename="qml/QRCodeShow.qml" line="90"/>
         <source>QR-code share</source>
+        <oldsource>Example Share</oldsource>
         <extracomment>Header for QR-code plugin</extracomment>
-        <translation>QR-kod del</translation>
+        <translation>QR-code share</translation>
+    </message>
+</context>
+<context>
+    <name>QRUploader</name>
+    <message>
+        <location filename="src/qruploader.cpp" line="45"/>
+        <source>QR-code</source>
+        <translation>QR-kod</translation>
     </message>
 </context>
 </TS>
